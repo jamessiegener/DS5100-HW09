ac6781d601c66ee34406c8bfd4ddf25265b8fdf9
James Siegener <160677043+james-siegener@users.noreply.github.com>
2024-07-03 17:16:51 -0400

    Pushing updated package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
-setup(name='HW09',
-      version='0.1',
-      description='Package for HW09',
+setup(name='booklover',
+      version='0.2',
+      description='Package for booklover',
       url='https://github.com/jamessiegener/DS5100-HW09',
       author='James Siegener',
       author_email='gnq2mr@virginia.edu',
